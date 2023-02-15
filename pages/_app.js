06ca23794b7e887107e79c45f6f4b8171de164d8
Hitoshi <hitoshi.gaia.galaxy@gmail.com>
2023-02-15 20:30:36 +0900

    ユーザー登録・ログイン

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,59 @@ import App from 'next/app'
 import Head from 'next/head'
 import { Layout } from '../components/Layout'
 import withData from '../lib/apollo'
+import AppContext from '../context/AppContext'
+import Cookies from 'js-cookie'
 
 class MyApp extends App {
+  // const [state, setState]=useState(null)
+
+  state = {
+    user: null,
+  }
+  setUser = (user) => {
+    this.setState({ user })
+  }
+
+  // すでにクッキー情報が残っているかを確認する
+  componentDidMount() {
+    const token = Cookies.get('token') //tokenの中にjwtが入っている
+
+    if (token) {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then(async (res) => {
+        if (!res.ok) {
+          Cookies.remove('token')
+          this.setState({ user: null })
+          return null
+        }
+        const user = await res.json()
+        this.setUser(user) //ログインと同等
+      })
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props
     return (
-      <>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-          />
-        </Head>
+      <AppContext.Provider
+        value={{ user: this.state.user, setUser: this.setUser }}
+      >
+        <>
+          <Head>
+            <link
+              rel="stylesheet"
+              href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+            />
+          </Head>
 
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </>
+      </AppContext.Provider>
     )
   }
 }
